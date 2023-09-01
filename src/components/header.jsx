dddf41129da99906ad87e97e8e543fe1b5cefd3e
Angel207207 <holosha.aleksandr@mail.ru>
2023-09-01 04:35:24 +0400

    "It's the first git-version"

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://i.pinimg.com/originals/07/26/68/07266852e902697a958cb9fec2815780.png"></img>
+    </header>
+};
+
+export default Header;
---
+++ src/components/profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="profile">
+        <div>
+            <img src="https://catherineasquithgallery.com/uploads/posts/2021-03/1614638157_46-p-kosmicheskii-fon-dlya-fotoshopa-77.jpg"></img>
+        </div>
+        <div>Ava + Description</div>
+        <div>My Posts</div>
+        <div>New Post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+    </div>
+};
+
+export default Profile;
