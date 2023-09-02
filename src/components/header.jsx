bb4bd3a2717af3056d2009ae4f2be9e9be410347
Angel207207 <holosha.aleksandr@mail.ru>
2023-09-02 04:52:22 +0400

    "Added CSS modules Ex.14"

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from "./header.module.css";
 
 const Header = () => {
-    return <header className="header">
+    return <header className={s.header}>
         <img src="https://i.pinimg.com/originals/07/26/68/07266852e902697a958cb9fec2815780.png"></img>
     </header>
 };
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import s from "./profile.module.css";
 
 const Profile = () => {
-    return <div className="profile">
+    return <div className={s.profile}>
         <div>
             <img src="https://catherineasquithgallery.com/uploads/posts/2021-03/1614638157_46-p-kosmicheskii-fon-dlya-fotoshopa-77.jpg"></img>
         </div>
         <div>Ava + Description</div>
         <div>My Posts</div>
         <div>New Post</div>
-        <div>Post 1</div>
-        <div>Post 2</div>
+        <div className={s.posts}>
+            <div className={s.item}>Post 1</div>
+            <div className={s.item}>Post 2</div>
+        </div>
     </div>
 };
 
