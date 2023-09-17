542397608e60e6c45998913229685065efab836d
Angel207207 <holosha.aleksandr@mail.ru>
2023-09-17 22:43:54 +0400

    A little bug fixes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import s from "./header.module.css";
 
 const Header = () => {
-    return <header className={s.header}>
-        <img src="https://i.pinimg.com/originals/07/26/68/07266852e902697a958cb9fec2815780.png"></img>
+  return (
+    <header className={s.header}>
+      <img src="https://i.pinimg.com/originals/07/26/68/07266852e902697a958cb9fec2815780.png"></img>
     </header>
+  );
 };
 
 export default Header;
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import s from "./profile.module.css";
 
 const Profile = () => {
-    return <div className={s.profile}>
-        <div>
-            <img src="https://catherineasquithgallery.com/uploads/posts/2021-03/1614638157_46-p-kosmicheskii-fon-dlya-fotoshopa-77.jpg"></img>
-        </div>
-        <div>Ava + Description</div>
-        <div>My Posts</div>
-        <div>New Post</div>
-        <div className={s.posts}>
-            <div className={s.item}>Post 1</div>
-            <div className={s.item}>Post 2</div>
-        </div>
+  return (
+    <div className={s.profile}>
+      <div>
+        <img src="https://catherineasquithgallery.com/uploads/posts/2021-03/1614638157_46-p-kosmicheskii-fon-dlya-fotoshopa-77.jpg"></img>
+      </div>
+      <div>Ava + Description</div>
+      <div>My Posts</div>
+      <div>New Post</div>
+      <div className={s.posts}>
+        <div className={s.item}>Post 1</div>
+        <div className={s.item}>Post 2</div>
+      </div>
     </div>
+  );
 };
 
 export default Profile;
